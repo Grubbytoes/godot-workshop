6008b83d05bf852988da141ac59f0913d39a72fe
hughie <=>
2025-02-18 14:38:53 +0000

    new style animation

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,14 +1,13 @@
 extends RigidBody2D
 
-@onready var anim: AnimationPlayer = $Anim
-@onready var sprite: Sprite2D = $Sprite
+@onready var sprite: AnimatedSprite2D = $Sprite
 
 @export var move_speed = 50
 @export var jump_height = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim.play("idle")
+	sprite.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -19,9 +18,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		movement_input.x += -1
 		sprite.flip_h = true
-	if Input.is_action_pressed("ui_right"):
+		sprite.play("run")
+	elif Input.is_action_pressed("ui_right"):
 		movement_input.x += 1
 		sprite.flip_h = false
+		sprite.play("run")
+	else:
+		sprite.play("idle")
 	apply_force(movement_input * move_speed)
 	#linear_velocity = movement_input * move_speed
 	
