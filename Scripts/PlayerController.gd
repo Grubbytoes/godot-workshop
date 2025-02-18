255824d8dbda8cef28cfc535fecf258ae4a863d2
hughie <=>
2025-02-18 14:27:25 +0000

    old timeline animation

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
-@export var move_speed = 50;
-@export var jump_height = 200;
+@onready var anim: AnimationPlayer = $Anim
+@onready var sprite: Sprite2D = $Sprite
+
+@export var move_speed = 50
+@export var jump_height = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	anim.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -15,8 +18,10 @@ func _physics_process(delta):
 	# left right movement
 	if Input.is_action_pressed("ui_left"):
 		movement_input.x += -1
+		sprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
 		movement_input.x += 1
+		sprite.flip_h = false
 	apply_force(movement_input * move_speed)
 	#linear_velocity = movement_input * move_speed
 	
