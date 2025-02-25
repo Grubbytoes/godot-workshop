f5dc8a5747c299d7d4129fd5ba82e5ed2fcdbddb
hughie <=>
2025-02-25 11:24:28 +0000

    Added collectables

---
+++ Scripts/collectable.gd
@@ -0,0 +1,22 @@
+class_name node
+extends Node2D
+
+@onready var sprite: AnimatedSprite2D = $Sprite
+
+func _ready():
+	sprite.play('default')
+
+
+func picked_up(body):
+	if not body.is_in_group("player_pickup"):
+		return 
+	
+	sprite.play("picked_up")
+
+	var timer := Timer.new()
+	add_child(timer)
+	timer.timeout.connect(func():
+		print("Goodbye!")
+		queue_free()
+	)
+	timer.start()
