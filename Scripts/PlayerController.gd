55d2403fcb94e4d545918f72d736461fa9a5afaa
hughie <=>
2025-03-04 13:45:32 +0000

    Spring cleanin' gettin' ready for love x

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,33 +1,46 @@
-extends RigidBody2D
+extends CharacterBody2D
+
+const PLAYER_MOVE_SPEED = 120
+const PLAYER_JUMP_FORCE = 250
+
+var move_dir: Vector2 
 
 @onready var sprite: AnimatedSprite2D = $Sprite
 
-@export var move_speed = 1000
-@export var jump_height = 555
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite.play("idle")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var movement_input = Vector2()
-	
+	move_dir = Vector2.ZERO
 	
 	# left right movement
 	if Input.is_action_pressed("ui_left"):
-		movement_input.x += -1
+		move_dir.x += -1
 		sprite.flip_h = true
 		sprite.play("run")
 	elif Input.is_action_pressed("ui_right"):
-		movement_input.x += 1
+		move_dir.x += 1
 		sprite.flip_h = false
 		sprite.play("run")
 	else:
 		sprite.play("idle")
-	apply_force(movement_input * move_speed)
-	#linear_velocity = movement_input * move_speed
 	
+	velocity.x += move_dir.x * PLAYER_MOVE_SPEED * delta * 4
+	velocity.x = clamp(velocity.x, -PLAYER_MOVE_SPEED, PLAYER_MOVE_SPEED)
+
 	# Jumping
 	if Input.is_action_just_pressed("ui_up"):
-		apply_impulse(Vector2(0,-1) * jump_height)
+		velocity.y -= PLAYER_JUMP_FORCE	
+	
+	# Gravity
+	velocity.y += 500 * delta
+
+	# Drag
+	if move_dir.x == 0:
+		if 1 < abs(velocity.x):
+			velocity.x = lerp(velocity.x, 0.0, 0.15)
+		else:
+			velocity.x = 0
+	
+
+	move_and_slide()
