87093df80a35c7f18ef54363ff3f80d36b392487
hughie <=>
2025-03-11 13:38:05 +0000

    snail now works

--- Scripts/enemy_snail.gd
+++ Scripts/enemy_snail.gd
@@ -4,6 +4,8 @@ const MOVE_SPEED = 40
 
 var x_dir: int
 
+@onready var line_of_sight = $LineOfSight
+
 func _ready():
 	x_dir = -1
 	sprite.play("default")
@@ -19,13 +21,22 @@ func _physics_process(delta):
 
 
 func do_move():
+	if not is_on_floor():
+		velocity.x = 0
+		return
+		
 	velocity.x = x_dir * MOVE_SPEED
 
 
 func turn():
 	x_dir *= -1
+	line_of_sight.scale.x = -x_dir
 
 	if 0 < x_dir:
 		sprite.flip_h = true
 	else:
 		sprite.flip_h = false
+
+
+func body_entered_sight(body):
+	turn()
