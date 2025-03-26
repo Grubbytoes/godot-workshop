962e4912e3ea2627ccc71469507aedd9e28f8225
Eddie <edwardpaullynch@gmail.com>
2025-03-26 14:55:00 +0000

    Godot Workshop 25/03/2025

--- Scripts/standard_character.gd
+++ Scripts/standard_character.gd
@@ -3,11 +3,15 @@ extends CharacterBody2D
 
 const CHARACTER_GRAVITY = 500
 
+@export var healthPoints = 1
+
 @onready var sprite: AnimatedSprite2D = $Sprite
 @onready var poof := load("res://Scenes/Prefabs/poof.tscn")
 func apply_gravity(delta):
 	velocity.y += CHARACTER_GRAVITY * delta
 
+func take_damage(amount:int):
+	healthPoints -= amount
 
 func die():
 	print("dead")
